37bcb2f294f5d88d00c46c057a0641548f59d1c8
Francisco Alexandre  3 <franciscoalexandrefernandesofc@gmail.com>
2022-04-18 08:50:25 -0300

    Add files via upload

---
+++ quiz/js/index.js
@@ -0,0 +1,232 @@
+let select = document.querySelector("#Dhelmise");
+        select.addEventListener("change", setDhelmise);
+
+        function setDhelmise() {
+
+            if (select.value == "Certo") {
+                pontos += 10
+            } else if (select.value == "Errado") {
+            }
+        }
+
+
+let result = document.querySelector('#result')
+let verificarResultado = document.querySelector('#verificarResultado')
+verificarResultado.addEventListener('click', verificar)
+
+function verificar(){
+    console.log("Verificar: " + pontos)
+    
+    result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+}
+let select1 = document.querySelector("#Eternatus");
+        select1.addEventListener("change", setEternatus);
+
+        function setEternatus() {
+
+            if (select1.value == "Errado") {
+         
+            } else if (select1.value == "Certo") {
+                pontos += 10
+            }
+        }
+
+let result1 = document.querySelector('#result')
+let verificarResultado1 = document.querySelector('#verificarResultado')
+verificarResultado1.addEventListener('click', verificar)
+
+function verificar(){
+    console.log("Verificar: " + pontos)
+    
+    result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+}
+let select2 = document.querySelector("#Steven");
+        select2.addEventListener("change", setSteven);
+
+        function setSteven() {
+        
+            if (select2.value == "Errado") {
+              
+            } else if (select2.value == "Certo") {
+                pontos += 10
+            }
+        }
+let result2 = document.querySelector('#result')
+let verificarResultado2 = document.querySelector('#verificarResultado')
+verificarResultado2.addEventListener('click', verificar)
+
+function verificar(){
+    console.log("Verificar: " + pontos)
+    
+    result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+}
+let select3 = document.querySelector("#Diantha");
+select3.addEventListener("change", setDiantha);
+
+function setDiantha() {
+
+    if (select3.value == "Gardevoir") {
+        pontos += 10
+
+    } else if (select3.value == "Gallade") {
+    }
+    else if (select3.value == "Clefable") {
+    }
+    else if (select3.value == "Mr.Mime") {
+     
+    }
+}
+let result3 = document.querySelector('#result')
+let verificarResultado3 = document.querySelector('#verificarResultado')
+verificarResultado3.addEventListener('click', verificar)
+
+function verificar(){
+    console.log("Verificar: " + pontos)
+    
+    result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+}
+let select4 = document.querySelector("#Palkia");
+        select4.addEventListener("change", setPalkia);
+
+        function setPalkia() {
+
+            if (select4.value == "Kanto") {
+
+            } else if (select4.value == "Kalos") {
+            }
+            else if (select4.value == "Sinnoh") {
+                pontos += 10
+            }
+            else if (select4.value == "Alola") {
+            }
+        }
+        let result4 = document.querySelector('#result')
+        let verificarResultado4 = document.querySelector('#verificarResultado')
+        verificarResultado4.addEventListener('click', verificar)
+        
+        function verificar(){
+            console.log("Verificar: " + pontos)
+            
+            result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+        }
+        let select5 = document.querySelector("#TrioDoLago");
+        select5.addEventListener("change", setTrioDoLago);
+
+        function setTrioDoLago() {
+
+            if (select5.value == "Azelf") {
+                pontos += 10
+
+            } else if (select5.value == "Raikou") {
+            }
+            else if (select5.value == "Giratina") {
+            }
+            else if (select5.value == "Moltres") {
+            }
+        }
+        let result5 = document.querySelector('#result')
+        let verificarResultado5 = document.querySelector('#verificarResultado')
+        verificarResultado5.addEventListener('click', verificar)
+        
+        function verificar(){
+            console.log("Verificar: " + pontos)
+            
+            result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+        }
+        let select6 = document.querySelector("#TeamGalatic");
+        select6.addEventListener("change", setTeamGalatic);
+
+        function setTeamGalatic() {
+
+            if (select6.value == "Red") {
+
+            } else if (select6.value == "Giovanni") {
+            }
+            else if (select6.value == "Cyrus") {
+                pontos += 10
+            }
+            else if (select6.value == "Cynthia") {
+            }
+        }
+        let result6 = document.querySelector('#result')
+        let verificarResultado6 = document.querySelector('#verificarResultado')
+        verificarResultado6.addEventListener('click', verificar)
+        
+        function verificar(){
+            console.log("Verificar: " + pontos)
+            
+            result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+        }
+       let select7 = document.querySelector("#LigaKalos");
+        select7.addEventListener("change", setLigaKalos);
+
+        function setLigaKalos() {
+
+            if (select7.value == "Iris") {
+
+            } else if (select7.value == "Blue") {
+            }
+            else if (select7.value == "Gary") {
+            }
+            else if (select7.value == "Alain") {
+                pontos += 10
+            }
+        }
+        let result7 = document.querySelector('#result')
+        let verificarResultado7 = document.querySelector('#verificarResultado')
+        verificarResultado7.addEventListener('click', verificar)
+        
+        function verificar(){
+            console.log("Verificar: " + pontos)
+            
+            result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+        }
+        let select8 = document.querySelector("#Pseudo");
+        select8.addEventListener("change", setPseudo);
+
+        function setPseudo() {
+
+            if (select8.value == "Salamance") {
+
+            } else if (select8.value == "Haxorus") {
+                pontos += 10
+            }
+            else if (select8.value == "Kommo-O") {
+            }
+            else if (select8.value == "Hydreigon") {
+            }
+        }
+        let result8 = document.querySelector('#result')
+        let verificarResultado8 = document.querySelector('#verificarResultado')
+        verificarResultado8.addEventListener('click', verificar)
+        
+        function verificar(){
+            console.log("Verificar: " + pontos)
+            
+            result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+        }
+        let select9 = document.querySelector("#Mega");
+        select9.addEventListener("change", setMega);
+
+        function setMega() {
+
+            if (select9.value == "Keldeo") {
+                pontos += 10
+
+            } else if (select9.value == "Mewtwo") {
+            }
+            else if (select9.value == "Rayquaza") {
+            }
+            else if (select9.value == "Diancie") {
+            }
+        }
+        let result9 = document.querySelector('#result')
+        let verificarResultado9 = document.querySelector('#verificarResultado')
+        verificarResultado9.addEventListener('click', verificar)
+        
+        function verificar(){
+            console.log("Verificar: " + pontos)
+            
+            result.innerHTML = "<h2>Resultado </h2><br> Pontos: " + pontos
+        }
+        
